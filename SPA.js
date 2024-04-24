5853f2de62df334e33e805cf55052fbc3e0b9aef
Ajay Kumar Addike <ajaykumar.addike@gmail.com>
2024-04-23 23:48:48 -0400

    added reports

--- SPA.js
+++ SPA.js
@@ -1,7 +1,7 @@
-var mainContent = document.getElementById('main-content');
+var mainContent = document.getElementById("main-content");
 
-window.loadHomePage = function() {
-mainContent.innerHTML = `
+window.loadHomePage = function () {
+  mainContent.innerHTML = `
     <div class="row justify-content-center mt-4">
     <div class="col-md-6">
         <div class="card bg-light mb-3">
@@ -17,21 +17,21 @@ mainContent.innerHTML = `
     </div>
     </div>
 `;
-initTypewriter(); 
+  initTypewriter();
 };
 
-window.loadStoryPage = function() {
-    mainContent.innerHTML = `
+window.loadStoryPage = function () {
+  mainContent.innerHTML = `
         <div class="row justify-content-center mt-4">
             <div class="col-md-6">
                 <div class="card bg-light mb-3">
                     <div class="card-body">
                         <div class="card-header">Story</div>
-                        <p id="demo" data-text="Adrian Bell, CTO of a large technology company was found to be missing after a gala event on April 1st, 2023, prompting an urgent police investigation. As a detective you are given access to the crime reports and other related databases to find the culprit."></p>
+                        <p id="demo" class="card-text" data-text="Adrian Bell, the CTO of a large technology company, was found to be missing after a gala event on April 1st, 2023, prompting an urgent police investigation. It has been three weeks, and there is no clue about Mr. Bell's whereabouts. The police have escalated the case to you. As a detective, you are now given access to the crime reports and other related databases to find the culprit."></p>
                         <div class="d-flex justify-content-between">
                             <button type="button" class="btn btn-success" onclick="loadHomePage()">Go Back</button>
-                            <button type="button" class="btn btn-success" >
-                            <a href="queries.html">GO!</a>
+                            <button type="button" class="btn btn-success" onclick="loadReportsPage()">
+                            Get Reports !
                             </button>
                         </div>
                     </div>
@@ -39,27 +39,88 @@ window.loadStoryPage = function() {
             </div>
         </div>
     `;
-    initTypewriter(); 
+  initTypewriter();
 };
 
+window.loadReportsPage = function () {
+  mainContent.innerHTML = `
+    <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <div
+          id="carouselExampleIndicators"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="img/Table1.png" class="d-block w-100" alt="Slide 1" />
+            </div>
+
+            <div class="carousel-item">
+              <img src="img/Table2.png" class="d-block w-100" alt="Slide 2" />
+            </div>
+
+            <div class="carousel-item">
+              <img src="img/Table3.png" class="d-block w-100" alt="Slide 4" />
+            </div>
+
+            <div class="carousel-item">
+              <img src="img/Table4.png" class="d-block w-100" alt="Slide 3" />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true">
+            </span>
+            <span class="visually-hidden"> Previous </span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true">
+            </span>
+            <span class="visually-hidden"> Next </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-between">
+                            <button type="button" class="btn btn-success" onclick="loadStoryPage()">Go Back</button>
+                            <button type="button" class="btn btn-success">
+                            <a href="queries.html">
+                            Go!
+                            </a>
+                            </button>
+                        </div>
+  </div>
+    `;
+  initTypewriter();
+};
 
 function initTypewriter() {
-var demo = document.getElementById("demo");
-var txt = demo.getAttribute("data-text");
-var i = 0;
-var speed = 7;
+  var demo = document.getElementById("demo");
+  var txt = demo.getAttribute("data-text");
+  var i = 0;
+  var speed = 7;
 
-function typeWriter() {
+  function typeWriter() {
     if (i < txt.length) {
-        demo.innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+      demo.innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
-}
-typeWriter();
+  }
+  typeWriter();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    loadHomePage(); // Initial load
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  loadHomePage(); // Initial load
+});
